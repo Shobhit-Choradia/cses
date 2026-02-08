c58c53fe8103c98789b515e3eeb636f83dc399d2
ShobhitPC <shobhit.p.choradia@gmail.com>
2026-02-08 23:42:00 +0530

    SLiding Window Sum Problem

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,5 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner (System.in);
-
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        long x = sc.nextLong();
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-
-        long[] arr = new long[n];
-        arr[0] = x;
-        for (int i = 1; i < n; i++){
-            arr[i] = (a*arr[i-1] + b) % c;
-        }
-
-        long winsum = 0;
-        for(int i = 0; i < k; i++){
-            winsum += arr[i];
-        }
-
-        long xor = winsum;
-        for(int i = k ; i < n; i++){
-            winsum += arr[i] - arr[i-k];
-            xor = xor ^ winsum;
-        }
-
-        System.out.println(xor);
     }
 }
