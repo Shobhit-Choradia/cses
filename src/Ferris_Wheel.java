012eb7ca50dae354384c098cc4115c749b35e34c
ShobhitPC <shobhit.p.choradia@gmail.com>
2026-02-12 15:01:04 +0530

    Ferris Wheel Problem

--- src/Ferris_Wheel.java
+++ src/Ferris_Wheel.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Ferris_Wheel {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner (System.in);
